Net-Geometry/picoclaw

fix: avoid duplicate v in CLI help banner

cmd/picoclaw/main.go
@@ -29,7 +29,7 @@ import (
 )
 
 func NewPicoclawCommand() *cobra.Command {
-	short := fmt.Sprintf("%s picoclaw - Personal AI Assistant v%s\n\n", internal.Logo, config.GetVersion())
+	short := fmt.Sprintf("%s picoclaw - Personal AI Assistant %s\n\n", internal.Logo, config.GetVersion())
 
 	cmd := &cobra.Command{
 		Use:     "picoclaw",

halfmoon-labs/halfmoon

cmd/halfmoon/main.go
@@ -27,7 +27,7 @@ import (
 )
 
 func NewHalfmoonCommand() *cobra.Command {
-	short := fmt.Sprintf("%s halfmoon - Personal AI Assistant v%s\n\n", internal.Logo, config.GetVersion())
+	short := fmt.Sprintf("%s halfmoon - Personal AI Assistant %s\n\n", internal.Logo, config.GetVersion())
 
 	cmd := &cobra.Command{
 		Use:     "halfmoon",